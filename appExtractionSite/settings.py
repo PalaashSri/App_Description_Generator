1fc6e8322e2e16b9998f63276c047517a99b32df
PalaashSri <palaash.srivastava@gmail.com>
2022-10-17 16:13:41 +1100

    Removing key from data

--- appExtractionSite/settings.py
+++ appExtractionSite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hlb(=^6&an@7*rn=%9jz!z@7_oatyk13%3el&)9cck=6l=gdii'
+SECRET_KEY = "INSERT_SECRET_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
